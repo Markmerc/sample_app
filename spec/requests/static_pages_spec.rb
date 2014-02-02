b31e6d9a94cccc6d931f95cb33dfc6f9cb90b9bd
Mark <mark.mercurio@gmail.com>
2014-02-02 14:24:34 -0500

    Updated tests for contact us

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -27,6 +27,17 @@ describe "StaticPages" do
     end
   end
   
+  describe "Contact page" do
+    
+    it "should have the h1 'Contact Us' " do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact Us")
+    end
+  end
   
   describe "About page" do    
     it "should have the h1 'About Us' " do
