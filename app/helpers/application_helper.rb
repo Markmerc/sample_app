4a69d0035fe6d60668f0c42e5d67ad787deb50b9
Mark <mark.mercurio@gmail.com>
2014-02-02 14:59:37 -0500

    Fixed Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   #Returns the full title on a per-page basis.
-  def full_tutle(page_title)
+  def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
-      base_tutle
+      base_title
     else
       "#{base_title} | #{page_title}"
     end
